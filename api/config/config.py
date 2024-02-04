219cc149c44f9d735263c4263be58257f707bfb7
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-02-04 16:50:57 +0000

    feat: Add migrations and URI

--- api/config/config.py
+++ api/config/config.py
@@ -1,12 +1,20 @@
+import os.path
+
 from decouple import config
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = config('SECRET_KEY', default='secret-key')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', default=False)
 
 
 class DevConfig(Config):
     DEBUG = config('DEBUG', default=True, cast=bool)
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI',
+                                     default='sqlite:///database.db')
+    SQLALCHEMY_ECHO = config('SQLALCHEMY_ECHO', default=True, cast=bool)
 
 
 class TestConfig(Config):

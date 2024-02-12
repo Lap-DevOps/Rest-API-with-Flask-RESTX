41914f5dc64ac53dbecb39b215fc74f9de609bc2
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-02-12 16:57:11 +0000

    feat: errors handlers

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_restx import Api
+from werkzeug.exceptions import NotFound, MethodNotAllowed
 
 from api.auth.views import auth_namespace
 from api.config.config import config_dict
@@ -26,6 +27,15 @@ def create_app(config=config_dict['dev']):
     )
     db.init_app(app)
     migrate = Migrate(app, db)
+
+
+    @api.errorhandler(NotFound)
+    def not_found(error):
+        return {"error": "Not found"}, 404
+
+    @api.errorhandler(MethodNotAllowed)
+    def method_not_allowed(error):
+        return {"error": "Method not allowed"}, 405
 
     api.add_namespace(auth_namespace, path="/auth")
     api.add_namespace(order_namespace, path="/orders")
